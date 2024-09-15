774b09a9c8f9386f657f445d7e1c0f8c501e3dca
Asim <asimsalim749@gmail.com>
2024-09-15 23:33:39 +0530

    new coorections

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,17 +3,18 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component'; // Adjust the path accordingly
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserUpdateComponent } from './user/user-update/user-update.component';
+import { CommonModule } from '@angular/common';
 // import { UserListComponent } from './user/user-list/user.list.component';
-import { HttpClient } from '@angular/common/http';
+ 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,UserCreateComponent,UserUpdateComponent], 
-   
+  imports: [RouterOutlet, HeaderComponent,UserCreateComponent,UserUpdateComponent,CommonModule], 
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'my-angular-app';
+  isCreateRoute: boolean = false;
 }
--- src/app/user/user-list/user.list.component.ts
+++ src/app/user/user-list/user.list.component.ts
@@ -1,33 +1,32 @@
-// // src/app/views/user/user-list.component.ts
-// import { Component, OnInit } from '@angular/core';
-// import { UserService } from '../../services/user.service';
-// import { User } from '../../model/user.model';
-// import { RouterModule } from '@angular/router';
-// import { CommonModule } from '@angular/common';
-// import { HttpClient } from '@angular/common/http';
+// src/app/views/user/user-list.component.ts
+import { Component, OnInit } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'; // Import HttpClientModule
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { UserService } from '../../services/user.service';
+import { User } from '../../model/user.model';
 
-// @Component({
-//   selector: 'app-user-list',
-//   standalone:true,
-//   imports: [RouterModule,CommonModule],
- 
-//   templateUrl: './user-list.component.html',
-//   // styleUrls: ['./user-list.component.css']
-// })
-// export class UserListComponent implements OnInit {
-//   users: User[] = [];
+@Component({
+  selector: 'app-user-list',
+  standalone: true,
+  imports: [CommonModule, RouterModule],  
+  templateUrl: './user-list.component.html',
+  // styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent implements OnInit {
+  users: User[] = [];
 
-//   constructor(private http: HttpClient) {}
+  constructor(private userService: UserService) {}  
 
-//   // ngOnInit(): void {
-//   //   this.userService.getUsers().subscribe(data => {
-//   //     this.users = data;
-//   //   });
-//   // }
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe(data => {
+      this.users = data;
+    });
+  }
 
-//   // deleteUser(id: number): void {
-//   //   this.userService.deleteUser(id).subscribe(() => {
-//   //     this.users = this.users.filter(user => user.id !== id);
-//   //   });
-//   // }
-// }
+  deleteUser(id: number): void {
+    this.userService.deleteUser(id).subscribe(() => {
+      this.users = this.users.filter(user => user.id !== id);
+    });
+  }
+}
