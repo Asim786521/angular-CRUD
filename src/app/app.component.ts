f3e05a6f933f048705fa53767b1977fa3e1e311c
Asim <asimsalim749@gmail.com>
2024-09-15 23:46:56 +0530

    edit and create placement coorected

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component'; // Adjust the path accordingly
 import { UserCreateComponent } from './user/user-create/user-create.component';
 import { UserUpdateComponent } from './user/user-update/user-update.component';
@@ -16,5 +16,14 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent {
   title = 'my-angular-app';
+
+  constructor (private router:Router) {}
+
+
+  ngOnInit():void{
+  this.router.events.subscribe(()=>{
+    this.isCreateRoute=this.router.url.includes('/create')
+  })
+  }
   isCreateRoute: boolean = false;
 }
